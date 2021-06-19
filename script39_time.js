d9af42f8c9c8c7e7f8878ed4f699e47e1de99eb6
Alex <phoenix.trade2307@gmail.com>
2021-06-19 12:27:51 +0300

    unit_40_date

--- script39_time.js
+++ script39_time.js
@@ -1,24 +1,68 @@
-// setTimeout не обязательно помещать в переменную, она и так будет работать.
-// но в случае переменной в переменную мы получаем числовой идентификатор этого таймера
-const timerId = setTimeout(function (text) {
-  console.log(text);
-  console.log(timerId);
-}, 2000, 'Goodbuy');
-//синтаксис такой, что сначала она (setTimeout) принимает ту функцию (function()),
-//которая должна запуститься через определенный промежуток времени
-//Причем она принимает именно обьявление этой функции или ее название
-//но при этом ф-ция сразу не запускается, а вызовется только через время, которые задается вторым аргументом в мс
-//третий аргумент - это знв=ачения для аргумента function, которая вызывается
-
 const btn = document.querySelector('.btn');
-btn.addEventListener('click', () => {
-  // const timerTwo = setTimeout(logger, 3000);
-  const timerTwo = setInterval(logger, 3000); // выполняет функцию через каждые 3000мс
-});
+// let timerId;
+// let i = 0;
 
-clearInterval(timerTwo); // останавливает таймер с идетификатором timerTwo
+// btn.addEventListener('click', () => {
+//   // timerId = setTimeout(logger, 1000);
+//   timerId = setInterval(logger, 500);
+// });
 
-function logger() {
-  console.log('some text');
-  console.log(timerTwo);
+// function logger() { // отслеживаем сколько раз повторился интервал
+//   if (i === 3) {
+//     clearInterval(timerId);
+//   }
+//   console.log('some text');
+//   i++;
+// }
+
+// let id = setTimeout(function log() {
+//   console.log('Hello Alex');
+//   id = setTimeout(log, 500);
+// }, 500);
+
+function myAnimation() {
+  const elem = document.querySelector('.box');
+  let pos = 0; // стартовая позиция нашего элемента
+  //нужна ф-ция, которая будет запускаться с интервалом и меняь позицию нашего элемента
+
+  const id = setInterval(frame, 10);
+
+  function frame() {
+    if (pos == 300) { //точка остановки элемента
+      clearInterval();
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
 }
+btn.addEventListener('click', myAnimation);
+
+const now = new Date();
+console.log(now);
+
+// Методы даты
+
+console.log(now.setHours(40));
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+// console.log(now.getTimezoneOffset());
+
+let start = new Date();
+
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+}
+let end = new Date();
+// alert(`Цикл отработал за ${end - start} милисекунд`);
+console.log(`Цикл отработал за ${end - start} милисекунд`);
+
+let time = new Date(1000000);
+console.log(time);
